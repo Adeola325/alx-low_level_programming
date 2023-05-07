1930d9243608089c8d037aef9c8c5194f3eeab9c
Adeola325 <olabisiadeola325@yahoo.com>
2023-05-07 07:41:47 -0700

    Task ./5-string_toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,20 +3,18 @@
 /**
  * string_toupper - Changes all lowercase letters
  * of a string to uppercase.
- * @str: The string to be changed
+ * @str: The string to be changed.
  * Return: A pointer to the changed string.
  */
-
 char *string_toupper(char *str)
-
 {
-        int index = 0;
+	int index = 0;
 
-        while (str[index])
-        {
-        if (str[index] >= 'a' && str[index] <= 'z')
-        str[index] -= 32;
-        index++;
-        }
-        return (str);
+	while (str[index])
+	{
+	if (str[index] >= 'a' && str[index] <= 'z')
+	str[index] -= 32;
+	index++;
+	}
+	return (str);
 }
